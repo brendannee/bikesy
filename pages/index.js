052f7ca600e39cc1c4100d7e10ed31e9f1354b79
Brendan Nee <brendan@blinktag.com>
2018-01-10 20:11:30 -0800

    Favicon paths

--- pages/index.js
+++ pages/index.js
@@ -14,11 +14,10 @@ export default () => (
       <link href='https://api.mapbox.com/mapbox.js/v3.1.1/mapbox.css' rel='stylesheet' />
       <link rel="stylesheet" type="text/css" href="static/css/style.css" />
 
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-      <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
-      <link rel="manifest" href="/manifest.json" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="apple-touch-icon" sizes="180x180" href="static/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" href="static/favicon/favicon-32x32.png" sizes="32x32" />
+      <link rel="icon" type="image/png" href="static/favicon/favicon-16x16.png" sizes="16x16" />
+      <link rel="mask-icon" href="static/favicon/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="theme-color" content="#ffffff" />
 
       <script src="https://maps.google.com/maps/api/js?libraries=places"></script>
