22519d4394af07e3e5d53f54621157ebe1c458ac
Brendan Nee <brendan@blinktag.com>
2019-04-14 00:34:38 -0700

    Privacy Policy

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const config = require('./frontendconfig.json');
 module.exports = {
   exportPathMap: function() {
     return {
-      '/': { page: '/' }
+      '/': { page: '/' },
+      '/privacy-policy': { page: '/privacy-policy' }
     }
   },
   assetPrefix: process && process.env.NODE_ENV !== 'development' ? config.assetPrefix : undefined
