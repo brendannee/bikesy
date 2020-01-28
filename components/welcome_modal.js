07a38e40e92583925669708971d54b15f9c93d67
Brendan Nee <brendan@blinktag.com>
2020-01-28 00:55:32 -0800

    Static file path

--- components/welcome_modal.js
+++ components/welcome_modal.js
@@ -16,8 +16,8 @@ class WelcomeModal extends React.Component {
       >
         <div className="welcome-modal">
           <img
-            src="static/images/bikemapper-logo.png"
-            srcset="static/images/bikemapper-logo@2x.png 2x"
+            src="images/bikemapper-logo.png"
+            srcset="images/bikemapper-logo@2x.png 2x"
             alt="Bike Mapper Logo"
             className="welcome-logo"
           />
