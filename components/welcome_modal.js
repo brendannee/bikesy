1b511f36559d74133897ab20256beddd1adf2ed5
Brendan Nee <brendan@blinktag.com>
2020-10-06 16:17:57 -0700

    Update welcome_modal.js

--- components/welcome_modal.js
+++ components/welcome_modal.js
@@ -1,40 +1,26 @@
 const React = require('react')
-import PropTypes from 'prop-types'
 const Modal = require('react-modal')
 
-class WelcomeModal extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+Modal.setAppElement('body')
 
-  render() {
-    return (
-      <Modal
-        isOpen={this.props.showWelcomeModal}
-        onRequestClose={this.props.hideWelcomeModal}
-        contentLabel="Welcome"
-      >
-        <div className="welcome-modal">
-          <h1>Welcome to Bikesy</h1>
-          <ul>
-            <li>Click anywhere to set the start and end points for your trip</li>
-            <li>Drag-and-drop markers to recalculate the route</li>
-            <li>Use the address boxes on the left to enter a specific address or landmark</li>
-          </ul>
-          <button onClick={this.props.hideWelcomeModal} className="btn btn-primary">OK</button>
-        </div>
-      </Modal>
-    )
-  }
-
-  componentWillMount() {
-    Modal.setAppElement('body')
-  }
-}
-
-WelcomeModal.propTypes = {
-  showWelcomeModal: PropTypes.bool.isRequired,
-  hideWelcomeModal: PropTypes.func.isRequired
+const WelcomeModal = ({ showWelcomeModal, hideWelcomeModal }) => {
+  return (
+    <Modal
+      isOpen={showWelcomeModal}
+      onRequestClose={hideWelcomeModal}
+      contentLabel="Welcome"
+    >
+      <div className="welcome-modal">
+        <h1>Welcome to Bikesy</h1>
+        <ul>
+          <li>Click anywhere to set the start and end points for your trip</li>
+          <li>Drag-and-drop markers to recalculate the route</li>
+          <li>Use the address boxes on the left to enter a specific address or landmark</li>
+        </ul>
+        <button onClick={hideWelcomeModal} className="btn btn-primary">OK</button>
+      </div>
+    </Modal>
+  )
 }
 
 export default WelcomeModal
