cfd6341a6e6848abade84c74fed17d69acc91b1d
Brendan Nee <brendan@blinktag.com>
2020-10-06 19:31:13 -0700

    Modernize map

--- components/map.js
+++ components/map.js
@@ -1,126 +1,108 @@
-const React = require('react')
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from 'react'
 
 import { latlngIsWithinBounds, drawMap, updateStartMarker, updateEndMarker, updatePath, updateMapSize, toggleBikeLockerLayer } from '../lib/map'
 const config = require('../frontendconfig.json')
 
-class Map extends React.Component {
-  constructor(props) {
-    super(props)
+const Map = ({ isMobile, mobileView, height, startLocation, endLocation, path, assignStartLocation, assignEndLocation }) => {
+  const [legendVisible, setLegendVisible] = useState(!isMobile)
+  const [bikeLockersVisible, setBikeLockersVisible] = useState(false)
 
-    this.state = {
-      legendVisible: !this.props.isMobile,
-      bikeLockersVisible: false
-    }
-
-    this.handleMapClick = latlng => {
-      if (!this.props.startLocation) {
-        if (latlngIsWithinBounds(latlng)) {
-          this.props.assignStartLocation(latlng)
-        }
-      } else if (!this.props.endLocation) {
-        if (latlngIsWithinBounds(latlng)) {
-          this.props.assignEndLocation(latlng)
-        }
-      }
-    }
-
-    this.handleMarkerDrag = (latlng, type) => {
+  const handleMapClick = latlng => {
+    if (!startLocation) {
       if (latlngIsWithinBounds(latlng)) {
-        if (type === 'start') {
-          this.props.assignStartLocation(latlng)
-        } else if (type === 'end') {
-          this.props.assignEndLocation(latlng)
-        }
+        assignStartLocation(latlng)
+      }
+    } else if (!endLocation) {
+      if (latlngIsWithinBounds(latlng)) {
+        assignEndLocation(latlng)
       }
     }
+  }
 
-    this.toggleLegendVisibility = () => {
-      this.setState({
-        legendVisible: !this.state.legendVisible
-      })
-    }
-
-    this.toggleBikeLockerVisibility = () => {
-      this.setState({
-        bikeLockersVisible: !this.state.bikeLockersVisible
-      }, () => {
-        toggleBikeLockerLayer(this.state.bikeLockersVisible)
-      })
+  const handleMarkerDrag = (latlng, type) => {
+    if (latlngIsWithinBounds(latlng)) {
+      if (type === 'start') {
+        assignStartLocation(latlng)
+      } else if (type === 'end') {
+        assignEndLocation(latlng)
+      }
     }
   }
 
-  componentDidMount() {
-    drawMap(this.handleMapClick, this.handleMarkerDrag)
+  const toggleLegendVisibility = () => {
+    setLegendVisible(!legendVisible)
   }
 
-  static getDerivedStateFromProps(nextProps) {
-    updateStartMarker(nextProps.startLocation)
-    updateEndMarker(nextProps.endLocation)
-    updatePath(nextProps.path)
-
-    return null
+  const toggleBikeLockerVisibility = () => {
+    toggleBikeLockerLayer(!bikeLockersVisible)
+    setBikeLockersVisible(!bikeLockersVisible)
   }
 
-  componentDidUpdate() {
+  useEffect(() => {
+    drawMap(handleMapClick, handleMarkerDrag)
+  }, [])
+
+  useEffect(() => {
+    updateStartMarker(startLocation)
+  }, [startLocation])
+
+  useEffect(() => {
+    updateEndMarker(endLocation)
+  }, [endLocation])
+
+  useEffect(() => {
+    updatePath(path)
+  }, [path])
+
+  useEffect(() => {
     updateMapSize()
-  }
+  }, [height])
 
-  render() {
-    return (
-      <div
-        className="map-container"
-        hidden={this.props.isMobile && this.props.mobileView !== 'map'}
-      >
-        <div className="logo">
-          <img src="/images/bikesy-logo.png" srcSet="/images/bikesy-logo@2x.png 2x" alt="logo" />
-        </div>
-        <div className="map" id="map" style={{ height: `${this.props.height}px` }}></div>
-        { this.state.legendVisible ? (
-          <div className="map-layers d-print-none">
-            <div
-              className="close-box"
-              onClick={this.toggleLegendVisibility}
-            >&minus;</div>
-            <div>
-              <div className="map-legend-item" title="bike lockers">
-                <div className="map-legend-icon bike-lockers"></div>
-                <label>
-                  <input type="checkbox" value={this.state.bikeLockersVisible} onChange={this.toggleBikeLockerVisibility} /> Bike Lockers
-                </label>
-              </div>
-              <div className="map-legend-item" title="paved, separated (off the street) bikeways">
-                <div className="map-legend-icon class1"></div>
-                <label>Multi-use Path</label>
-              </div>
-              <div className="map-legend-item" title="dedicated on-street bikeways, marked by striping on pavement">
-                <div className="map-legend-icon class2"></div>
-                <label>Bike Lane</label>
-              </div>
-              <div className="map-legend-item" title="on-street routes signed for bicyclists">
-                <div className="map-legend-icon class3"></div>
-                <label>Bike Route</label>
-              </div>
+
+  return (
+    <div
+      className="map-container"
+      hidden={isMobile && mobileView !== 'map'}
+    >
+      <div className="logo">
+        <img src="/images/bikesy-logo.png" srcSet="/images/bikesy-logo@2x.png 2x" alt="logo" />
+      </div>
+      <div className="map" id="map" style={{ height: `${height}px` }}></div>
+      { legendVisible ? (
+        <div className="map-layers d-print-none">
+          <div
+            className="close-box"
+            onClick={toggleLegendVisibility}
+          >&minus;</div>
+          <div>
+            <div className="map-legend-item" title="bike lockers">
+              <div className="map-legend-icon bike-lockers"></div>
+              <label>
+                <input type="checkbox" value={bikeLockersVisible} onChange={toggleBikeLockerVisibility} /> Bike Lockers
+              </label>
+            </div>
+            <div className="map-legend-item" title="paved, separated (off the street) bikeways">
+              <div className="map-legend-icon class1"></div>
+              <label>Multi-use Path</label>
+            </div>
+            <div className="map-legend-item" title="dedicated on-street bikeways, marked by striping on pavement">
+              <div className="map-legend-icon class2"></div>
+              <label>Bike Lane</label>
+            </div>
+            <div className="map-legend-item" title="on-street routes signed for bicyclists">
+              <div className="map-legend-icon class3"></div>
+              <label>Bike Route</label>
             </div>
           </div>
-        ) : (
-          <div
-            className="map-layers-open-box"
-            onClick={this.toggleLegendVisibility}
-          >Toggle Map Legend</div>
-        )}
-      </div>
-    )
-  }
-}
-
-Map.propTypes = {
-  path: PropTypes.object,
-  startLocation: PropTypes.object,
-  endLocation: PropTypes.object,
-  height: PropTypes.number,
-  isMobile: PropTypes.bool,
-  mobileView: PropTypes.string
+        </div>
+      ) : (
+        <div
+          className="map-layers-open-box"
+          onClick={toggleLegendVisibility}
+        >Toggle Map Legend</div>
+      )}
+    </div>
+  )
 }
 
 export default Map
