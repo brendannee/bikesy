d685d7e1d3bb63764a209056264c80126da63883
Nick Speal <nick.speal+github@flyzipline.com>
2021-10-24 13:02:15 -0700

    Support custom configuration of Welcome Modal

--- src/components/welcome_modal.js
+++ src/components/welcome_modal.js
@@ -9,7 +9,7 @@ const WelcomeModal = ({ showWelcomeModal, hideWelcomeModal }) => {
       contentLabel="Welcome"
     >
       <div className="welcome-modal">
-        <h1>Welcome to Bikesy</h1>
+        <h1>{process.env.NEXT_PUBLIC_WELCOME_MODAL_TITLE || 'Welcome to Bikesy'}</h1>
         <ul>
           <li>Click anywhere to set the start and end points for your trip</li>
           <li>Drag-and-drop markers to recalculate the route</li>
