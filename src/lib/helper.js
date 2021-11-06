7bbf670055aab469e6b176ad0c488910a6e42dd1
Nick Speal <nick.speal+github@flyzipline.com>
2021-11-06 14:48:09 -0700

    Clean up getElevationGain to be more readable

--- src/lib/helper.js
+++ src/lib/helper.js
@@ -31,13 +31,15 @@ export function formatTime(miles) {
 }
 
 export function getElevationGain(profile) {
+  // Compute the sum of all the positive elevation gain segments along the route
+  // profile is a list of objects with "elevation" and "distance" properties, each corresponding to a path segment
   let totalElevGain = 0;
-  profile.forEach((_, idx) => {
-    if (idx < profile.length - 1 && profile[idx]['elevation'] < profile[idx + 1]['elevation']) {
-      totalElevGain += profile[idx + 1]['elevation'] - profile[idx]['elevation'];
+  for (let idx = 0; idx < (profile.length - 1); idx++) {
+    const elevationStep = profile[idx + 1]['elevation'] - profile[idx]['elevation']
+    if (elevationStep > 0) {
+      totalElevGain += elevationStep
     }
-  });
-
+  }
   return totalElevGain;
 }
 
