d6fb2d638471e7e3c0a4dfc4f5b29fbbb13cc117
robotnealan2 <robert.nealan@percipient.ai>
2021-11-26 14:32:33 -0800

    Add redux logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+
 import search from './slices/search';
 
 export default configureStore({
   reducer: {
     search,
   },
+  middleware: (getDefaultMiddlware) => getDefaultMiddlware().concat(logger),
 });
