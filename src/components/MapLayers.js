9cb70a9da3a6fdc7597145800e17c9582ce2f01b
Nick Speal <nick@speal.ca>
2022-04-08 23:57:11 -0700

    rm unused imports

--- src/components/MapLayers.js
+++ src/components/MapLayers.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 
 const MapLayers = ({ isInitiallyVisible }) => {
   const [isVisible, setisVisible] = useState(isInitiallyVisible);
