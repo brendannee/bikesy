996609aff6d088c3bef483cd712bc6fe9a9b6ef6
Brendan Nee <brendan@blinktag.com>
2023-04-05 23:38:22 -0700

    Update geocoding options

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -198,7 +198,7 @@ const Controls = ({
       });
     },
     options: {
-      types: ['geocode'],
+      types: [],
       bounds,
       fields: ['formatted_address', 'geometry.location'],
       strictBounds: true,
